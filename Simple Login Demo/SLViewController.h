9e93ea74778b414d81a3e9e7d516bd78fa1bd049
Jonny Dimond <jonny@firebase.com>
2014-08-11 17:52:33 +0200

    Initial Commit

---
+++ Simple Login Demo/SLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLViewController.h
+//  Simple Login Demo
+//
+//  Created by Jonny Dimond on 8/11/14.
+//  Copyright (c) 2014 Firebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLViewController : UIViewController
+
+@end
---
+++ Simple Login Demo/SLViewController.m
@@ -0,0 +1,29 @@
+//
+//  SLViewController.m
+//  Simple Login Demo
+//
+//  Created by Jonny Dimond on 8/11/14.
+//  Copyright (c) 2014 Firebase. All rights reserved.
+//
+
+#import "SLViewController.h"
+
+@interface SLViewController ()
+
+@end
+
+@implementation SLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
