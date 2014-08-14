992e5fb796387ea404e66d9a0f163a221544a2ed
Jonny Dimond <jonny@firebase.com>
2014-08-14 15:04:05 +0200

    removed commented out code from setup script

--- setup.sh
+++ setup.sh
@@ -39,34 +39,4 @@ download_sdk "Firebase SDK" "$FIREBASE_SDK_URL" "$FIREBASE_SDK_ZIP_FILE"
 download_sdk "Firebase Simple Login SDK" "$SIMPLE_LOGIN_SDK_URL" "$SIMPLE_LOGIN_SDK_ZIP_FILE"
 download_sdk "Google+ SDK" "$GOOGLE_SDK_URL" "$GOOGLE_SDK_ZIP_FILE"
 
-#if [ -f "$FIREBASE_SDK_ZIP_FILE" ]; then
-#    echo "Firebase zip already present. Skipping download..." 1>&2
-#else
-#    echo "Downloading Firebase SDK..." 1>&2
-#    curl "$FIREBASE_SDK_URL" -o "$FIREBASE_SDK_ZIP_FILE"
-#fi
-#
-#
-#if [ -d "$FIREBASE_SDK_DIR" ]; then
-#    echo "Firebase SDK already installed" 1>&2
-#else
-#    echo "Extracting Firebase SDK..." 1>&2
-#    unzip "$FIREBASE_SDK_ZIP_FILE" -d "$SDK_DIR"
-#fi
-#
-#if [ -f "$SIMPLE_LOGIN_SDK_ZIP_FILE" ]; then
-#    echo "Firebase Simple Login zip already present. Skipping download..." 1>&2
-#else
-#    echo "Downloading Firebase Simple Login SDK..." 1>&2
-#    curl "$SIMPLE_LOGIN_SDK_URL " -o "$SIMPLE_LOGIN_SDK_ZIP_FILE"
-#fi
-#
-#
-#if [ -d "$SIMPLE_LOGIN_SDK_DIR" ]; then
-#    echo "Firebase Simple Login SDK already installed" 1>&2
-#else
-#    echo "Extracting Firebase Simple Login SDK..." 1>&2
-#    unzip "$SIMPLE_LOGIN_SDK_ZIP_FILE" -d "$SDK_DIR"
-#fi
-
 echo "All done..." 1>&2
