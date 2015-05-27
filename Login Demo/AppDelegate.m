4fe131c9af977a6b9886f3b14643137252f25578
katfang <katfang@users.noreply.github.com>
2015-05-27 11:57:34 -0700

    Revert "Revert "Clean up""

---
+++ Login Demo/AppDelegate.m
@@ -0,0 +1,64 @@
+//
+//  AppDelegate.m
+//  Login Demo
+//
+//  Created by Katherine Fang on 5/26/15.
+//  Copyright (c) 2015 Firebase. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <GooglePlus/GooglePlus.h>
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                    didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBSDKAppEvents activateApp];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    // This is the Facebook or Google+ SDK returning to the app after authentication.
+    if ([url.scheme hasPrefix:@"fb"]) {
+        return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                              openURL:url
+                                                    sourceApplication:sourceApplication
+                                                           annotation:annotation];
+    } else {
+        return [GPPURLHandler handleURL:url
+                      sourceApplication:sourceApplication
+                             annotation:annotation];
+    }
+}
+
+
+@end
--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 This is a demo application to demonstrate how to use Firebase's authentication in
 your iOS app.
 
-To download and setup all necessary SDKs, please run `setup.sh`.
+To download and setup all necessary SDKs, please run `pod install`. This requires that
+[Cocoapods](https://cocoapods.org/) is installed. 
 
 You will need to create your own Firebase app and [enable the different
 authentication
 methods](https://www.firebase.com/docs/ios/guide/user-auth.html#section-providers)
-used in this app and update the values in `Simple Login
-Demo/SLViewController.m`.
+used in this app and update the values in `Login
+Demo/ViewController.m`.
